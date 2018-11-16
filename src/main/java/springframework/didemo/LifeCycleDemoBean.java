b400a543b848d7c13700628af6cddd67c1946e1d
SunilVasu <sunilkv24@gmail.com>
2018-11-15 19:43:09 -0600

    Spring Life Cycle

---
+++ src/main/java/springframework/didemo/LifeCycleDemoBean.java
@@ -0,0 +1,70 @@
+package springframework.didemo;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
+		BeanFactoryAware, ApplicationContextAware{
+
+	public LifeCycleDemoBean() {
+		System.out.println("## I am in the LifeCycleBeam Constructor");
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("## The LifeCycleBeam bean has been terminated");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("## The LifeCycleBeam has its properties set");
+	}
+	
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		System.out.println("## Bean factory has been set");
+	}
+	
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("## My Bean name is: "+name);
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		System.out.println("## Application context has been set");
+	}
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("## The Post Construct annotated method has been called");
+	}
+	@PreDestroy
+	public void preDestroy() {
+		System.out.println("## The Pre Destroy annotated method has been called");
+	}
+	
+	public void beforeInit() {
+		System.out.println("## Before init called by Bean Post Processor");
+	}
+	
+	public void afterInit() {
+		System.out.println("## After init called by Bean Post Processor");
+	}
+
+
+
+
+
+
+}
